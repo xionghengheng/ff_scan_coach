c78fb905e0353810ce1651007ac2770b683abcc0
derickwu <xionghengheng@gmail.com>
2024-09-13 23:51:04 +0800

    123
    go

--- get_coach_statistic.go
+++ get_coach_statistic.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/xionghengheng/ff_plib/comm"
 	"github.com/xionghengheng/ff_plib/db/dao"
+	"github.com/xionghengheng/ff_plib/db/model"
 	"net/http"
 	"time"
 )
@@ -24,15 +25,33 @@ type GetCoachStatisticRsp struct {
 }
 
 type CoachStatisticItem struct {
-	JoinTime     string `json:"join_time"`      // 教练入驻时间
-	CoachID      int    `json:"coach_id"`       // 教练ID
-	CoachName    string `json:"coach_name"`     //教练名称
-	Phone        string `json:"phone"`          //手机号
-	GymID        int    `json:"gym_id"`         //健身房id
-	Bio          string `json:"bio"`            //教练简介
-	RecReason    string `json:"rec_reason"`     //教练推荐原因
-	CourseIdList string `json:"course_id_list"` //教练可上课程列表，英文逗号分割
-	GoodAt       string `json:"good_at"`        //教练擅长领域
+	JoinTime      string            `json:"join_time"`           //教练入驻时间
+	CoachID       int               `json:"coach_id"`            //教练ID
+	CoachName     string            `json:"coach_name"`          //教练名称
+	Phone         string            `json:"phone"`               //手机号
+	GymID         int               `json:"gym_id"`              //健身房id
+	Bio           string            `json:"bio"`                 //教练简介
+	RecReason     string            `json:"rec_reason"`          //教练推荐原因
+	CourseIdList  string            `json:"course_id_list"`      //教练可上课程列表，英文逗号分割
+	GoodAt        string            `json:"good_at"`             //教练擅长领域
+	StatisticCalc StatisticCalcInfo `json:"statistic_calc_info"` //计算统计数据
+}
+
+// 计算统计数据
+type StatisticCalcInfo struct {
+	TrailPackageUv              int            `json:"trail_package_uv"`                // 体验用户数
+	TrailPackageUidList         map[int64]bool `json:"trail_package_uid_list"`          // 体验课课包用户数
+	TrailBookingCountUv         int            `json:"trail_booking_count_uv"`          // 体验约课人数
+	TrailBookingCountPv         int            `json:"trail_booking_count_pv"`          // 体验约课次数
+	TrailLessonWriteOffUv       int            `json:"trail_lesson_writeoff_uv"`        // 体验课核销人数
+	TrailLessonWriteOffPv       int            `json:"trail_lesson_writeoff_pv"`        // 体验课核销次数
+	PaidPackageUv               int            `json:"paid_package_uv"`                 // 正式课课包付费用户数
+	PaidPackageUidList          map[int64]bool `json:"paid_package_uid_list"`           // 正式课课包付费用户数
+	PaidPackageTotalLessonCount int            `json:"paid_package_total_lesson_count"` // 正式课付费课时次数
+	PaidPackageSalesRevenue     int            `json:"paid_package_sales_revenue"`      // 正式课付费销售额
+	PaidLessonWriteOffUv        int            `json:"paid_lesson_writeoff_uv"`         // 正式课核销人数
+	PaidLessonWriteOffPv        int            `json:"paid_lesson_writeoff_pv"`         // 正式课核销次数
+	PaidLessonWriteOffAmount    int            `json:"paid_lesson_writeoff_amount"`     // 正式课核销金额
 }
 
 func getGetCoachStatiticHandlerReq(r *http.Request) (GetCoachStatisticReq, error) {
@@ -102,6 +121,134 @@ func GetCoachStatiticHandler(w http.ResponseWriter, r *http.Request) {
 		rsp.TotalWriteOffLessonCnt += int64(v.LessonCnt)
 		rsp.TotalSales += int64(v.SaleRevenue)
 	}
+
+	mapCoachId2StatisticCalcInfo := make(map[int]StatisticCalcInfo)
+	for _, v := range mapCoach {
+		mapCoachId2StatisticCalcInfo[v.CoachID] = StatisticCalcInfo{}
+	}
+
+	var vecAllPackageModel []model.CoursePackageModel
+	var turnPageTs int64
+	for i := 0; i <= 5000; i++ {
+		tmpVecAllUserModel, err := dao.ImpCoursePackage.GetAllCoursePackageList(turnPageTs)
+		if err != nil {
+			rsp.Code = -911
+			rsp.ErrorMsg = err.Error()
+			Printf("GetAllCoursePackageList err, StatisticTs:%d err:%+v\n", req.StatisticTs, err)
+			return
+		}
+		if len(tmpVecAllUserModel) == 0 {
+			Printf("GetAllCoursePackageList empty, StatisticTs:%d vecAllPackageModel.len:%d\n", req.StatisticTs, len(vecAllPackageModel))
+			break
+		}
+		turnPageTs = tmpVecAllUserModel[len(tmpVecAllUserModel)-1].Ts
+		vecAllPackageModel = append(vecAllPackageModel, tmpVecAllUserModel...)
+	}
+
+	for _, v := range vecAllPackageModel {
+		if v.PackageType == model.Enum_PackageType_PaidPackage {
+			tmp := mapCoachId2StatisticCalcInfo[v.CoachId]
+			if _, ok := tmp.PaidPackageUidList[v.Uid]; !ok {
+				tmp.PaidPackageUv += 1
+				tmp.PaidPackageUidList[v.Uid] = true
+				mapCoachId2StatisticCalcInfo[v.CoachId] = tmp
+			}
+
+			tmp = mapCoachId2StatisticCalcInfo[v.CoachId]
+			tmp.PaidPackageTotalLessonCount += v.TotalCnt
+			tmp.PaidPackageSalesRevenue += v.Price
+			mapCoachId2StatisticCalcInfo[v.CoachId] = tmp
+		}
+	}
+
+	for _, v := range vecAllPackageModel {
+		if v.PackageType == model.Enum_PackageType_TrialFree {
+
+			//如果是付费用户，则免费课用户不计入
+			tmp := mapCoachId2StatisticCalcInfo[v.CoachId]
+			if _, ok := tmp.PaidPackageUidList[v.Uid]; ok {
+				continue
+			}
+
+			if _, ok := tmp.TrailPackageUidList[v.Uid]; !ok {
+				tmp.TrailPackageUv += 1
+				tmp.TrailPackageUidList[v.Uid] = true
+				mapCoachId2StatisticCalcInfo[v.CoachId] = tmp
+			}
+		}
+	}
+
+	var vecAllSingleLesson []model.CoursePackageSingleLessonModel
+	turnPageTs = 0
+	for i := 0; i <= 5000; i++ {
+		tmpVecAllSingleLesson, err := dao.ImpCoursePackageSingleLesson.GetAllSingleLessonList(turnPageTs)
+		if err != nil {
+			rsp.Code = -911
+			rsp.ErrorMsg = err.Error()
+			Printf("GetAllSingleLessonList err, StatisticTs:%d err:%+v\n", req.StatisticTs, err)
+			return
+		}
+		if len(tmpVecAllSingleLesson) == 0 {
+			Printf("GetAllSingleLessonList empty, StatisticTs:%d vecAllSingleLesson.len:%d\n", req.StatisticTs, len(vecAllSingleLesson))
+			break
+		}
+		turnPageTs = tmpVecAllSingleLesson[len(tmpVecAllSingleLesson)-1].CreateTs
+		vecAllSingleLesson = append(vecAllSingleLesson, tmpVecAllSingleLesson...)
+	}
+
+	//for _, v := range vecAllSingleLesson {
+	//	if v.Status == model.En_LessonStatusCompleted {
+	//		rsp.TotalClassesAttended += 1
+	//		rsp.TotalRedemptionAmount += int64(mapCourse[v.CourseID].Price)
+	//	}
+	//	if v.ScheduleBegTs > dayBegTs {
+	//		if v.Status == model.En_LessonStatusCompleted {
+	//			rsp.TodayCompletedClasses += 1
+	//		} else {
+	//			rsp.TodayBookedClasses += 1
+	//		}
+	//	}
+	//}
+	//
+	//for _, v := range vecAllSingleLesson {
+	//	if v.ScheduleBegTs < dayBegTs {
+	//		continue
+	//	}
+	//	var stLessonStatisticItem LessonStatisticItem
+	//	_, _, packageType := comm.ParseCoursePackageId(v.PackageID)
+	//	stCoachAppointmentModel, err := dao.ImpAppointment.GetAppointmentById(v.AppointmentID)
+	//	if err != nil {
+	//		Printf("GetAppointmentById err, err:%+v AppointmentID:%d\n", err, v.AppointmentID)
+	//	}
+	//	if stCoachAppointmentModel != nil {
+	//		t := time.Unix(stCoachAppointmentModel.CreateTs, 0)
+	//		stLessonStatisticItem.BookingTime = "课程发起预约的时间 " + t.Format("2006年01月02日 15:04")
+	//	}
+	//
+	//	stLessonStatisticItem.LessonID = v.LessonID
+	//	if packageType == model.Enum_PackageType_PaidPackage {
+	//		stLessonStatisticItem.LessonType = "正式课"
+	//	} else {
+	//		stLessonStatisticItem.LessonType = "体验课"
+	//	}
+	//
+	//	if v.Status == model.En_LessonStatus_Scheduled {
+	//		stLessonStatisticItem.LessonStatus = "已预约"
+	//	} else if v.Status == model.En_LessonStatusCompleted {
+	//		stLessonStatisticItem.LessonStatus = "已完成"
+	//	} else if v.Status == model.En_LessonStatusCanceled {
+	//		stLessonStatisticItem.LessonStatus = "已取消"
+	//	} else if v.Status == model.En_LessonStatusMissed {
+	//		stLessonStatisticItem.LessonStatus = "已旷课"
+	//	}
+	//	t := time.Unix(v.ScheduleBegTs, 0)
+	//	stLessonStatisticItem.ScheduleBegTs = "课程开始时间 " + t.Format("2006年01月02日 15:04")
+	//	t = time.Unix(v.WriteOffTs, 0)
+	//	stLessonStatisticItem.WriteOffTs = "课程核销时间 " + t.Format("2006年01月02日 15:04")
+	//
+	//	stLessonStatisticItem.CommentContent = v.CommentContent
+	//	rsp.LessonStatisticItemList = append(rsp.LessonStatisticItemList, stLessonStatisticItem)
+	//}
 
 	for _, v := range mapCoach {
 		var stCoachStatisticItem CoachStatisticItem
